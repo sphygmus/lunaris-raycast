0cfece383cf70697447ba416e5bde9a34db09c70
Sphygmus <kemalcangedik@mail.com>
2026-03-22 02:26:54 +0300

    Bug fixes & update to package.json

--- src/types/artifact.d.ts
+++ src/types/artifact.d.ts
@@ -1,39 +1,39 @@
 interface ArtifactPiece {
-  chsName: string;
-  enName: string;
-  icon: string;
-  ptName: string;
-  ruName: string;
+	chsName: string;
+	enName: string;
+	icon: string;
+	ptName: string;
+	ruName: string;
 }
 
 interface ArtifactSetBonuses {
-  "2pc": string;
-  "4pc": string;
+	"2pc": string;
+	"4pc"?: string;
 }
 
 interface ArtifactSet {
-  setId: number;
-  setIcon: string;
-  qualityType: GenshinQualityType;
-  releaseDate: number;
+	setId: number;
+	setIcon: string;
+	qualityType: GenshinQualityType;
+	releaseDate: number;
 
-  chsName: string;
-  enName: string;
-  ptName: string;
-  ruName: string;
+	chsName: string;
+	enName: string;
+	ptName: string;
+	ruName: string;
 
-  chsBonuses: ArtifactSetBonuses;
-  enBonuses: ArtifactSetBonuses;
-  ptBonuses: ArtifactSetBonuses;
-  ruBonuses: ArtifactSetBonuses;
+	chsBonuses: ArtifactSetBonuses;
+	enBonuses: ArtifactSetBonuses;
+	ptBonuses: ArtifactSetBonuses;
+	ruBonuses: ArtifactSetBonuses;
 
-  pieces: {
-    flower: ArtifactPiece;
-    plume: ArtifactPiece;
-    sands: ArtifactPiece;
-    goblet: ArtifactPiece;
-    circlet: ArtifactPiece;
-  };
+	pieces: {
+		flower: ArtifactPiece;
+		plume: ArtifactPiece;
+		sands: ArtifactPiece;
+		goblet: ArtifactPiece;
+		circlet: ArtifactPiece;
+	};
 }
 
 type ArtifactsMap = Record<string, ArtifactSet>;
