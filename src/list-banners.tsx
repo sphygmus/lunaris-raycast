516e7f39074b79b49593161e1ec68744f23eb938
Sphygmus <kemalcangedik@mail.com>
2026-03-23 21:26:37 +0300

    chore(prettier): remove printWidth to resolve linting conflict

--- src/list-banners.tsx
+++ src/list-banners.tsx
@@ -1,36 +1,52 @@
-import { Action, ActionPanel, getPreferenceValues, Grid, Icon } from "@raycast/api";
+import {
+  Action,
+  ActionPanel,
+  getPreferenceValues,
+  Grid,
+  Icon,
+} from "@raycast/api";
 import { useCachedPromise, usePromise } from "@raycast/utils";
-import { getAllCharacters, getBanners, getGameVersion } from "./lib/utils/lunaris";
+import {
+  getAllCharacters,
+  getBanners,
+  getGameVersion,
+} from "./lib/utils/lunaris";
 import { useEffect, useMemo, useState } from "react";
 import SingleCharacter from "./components/character/single-character";
 
 export default function Command() {
   const preferences = getPreferenceValues<Preferences>();
   const { data: characters } = usePromise(getAllCharacters);
-  const { isLoading, data: banners } = useCachedPromise(async (): Promise<BannerInformation[]> => {
-    const gameVersion = await getGameVersion();
-    const allBanners = await getBanners();
-    if (!allBanners) return [];
+  const { isLoading, data: banners } = useCachedPromise(
+    async (): Promise<BannerInformation[]> => {
+      const gameVersion = await getGameVersion();
+      const allBanners = await getBanners();
+      if (!allBanners) return [];
 
-    const bannerInformation: BannerInformation[] = Object.entries(allBanners.version)
-      .filter(([version]) => {
-        const vNum = parseFloat(version);
-        const gvNum = gameVersion ? parseFloat(gameVersion) : 0;
+      const bannerInformation: BannerInformation[] = Object.entries(
+        allBanners.version,
+      )
+        .filter(([version]) => {
+          const vNum = parseFloat(version);
+          const gvNum = gameVersion ? parseFloat(gameVersion) : 0;
 
-        if (!preferences.allowUnreleased && gvNum > 0 && vNum > gvNum) {
-          return false;
-        }
-        return true;
-      })
-      .map(([version, characters]) => ({
-        version,
-        characters,
-      }));
+          if (!preferences.allowUnreleased && gvNum > 0 && vNum > gvNum) {
+            return false;
+          }
+          return true;
+        })
+        .map(([version, characters]) => ({
+          version,
+          characters,
+        }));
 
-    return bannerInformation;
-  });
+      return bannerInformation;
+    },
+  );
 
-  const [selectedVersion, setSelectedVersion] = useState(banners && banners[0] ? banners[0].version : null);
+  const [selectedVersion, setSelectedVersion] = useState(
+    banners && banners[0] ? banners[0].version : null,
+  );
 
   useEffect(() => {
     if (banners && banners.length > 0 && !selectedVersion) {
@@ -51,9 +67,18 @@ export default function Command() {
       aspectRatio="9/16"
       navigationTitle={`Banners / ${selectedVersion || "Loading..."}`}
       searchBarAccessory={
-        <Grid.Dropdown tooltip="Select version" onChange={(newValue) => setSelectedVersion(newValue)}>
+        <Grid.Dropdown
+          tooltip="Select version"
+          onChange={(newValue) => setSelectedVersion(newValue)}
+        >
           {banners &&
-            banners.map(({ version }) => <Grid.Dropdown.Item key={version} title={version} value={version} />)}
+            banners.map(({ version }) => (
+              <Grid.Dropdown.Item
+                key={version}
+                title={version}
+                value={version}
+              />
+            ))}
         </Grid.Dropdown>
       }
     >
@@ -76,7 +101,12 @@ export default function Command() {
                 <Action.Push
                   title="Detailed Information"
                   icon={Icon.Person}
-                  target={<SingleCharacter id={id.toString()} character={characters[id.toString()]} />}
+                  target={
+                    <SingleCharacter
+                      id={id.toString()}
+                      character={characters[id.toString()]}
+                    />
+                  }
                 />
               </ActionPanel>
             }
